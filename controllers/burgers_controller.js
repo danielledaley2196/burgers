be83fa6e99cdeadfa291b5b07dfe3dc77214ba95
Danielle Daley <danielledaley2196@gmail.com>
2020-03-30 17:13:27 -0400

    routes added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,41 @@ let express = require("express");
 
 let router = express.Router();
 
+let burger = require("../models/burger.js");
 // code for routes here
 
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    let hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.push("/api/burgers", function(req, res) {
+  burger.creat(["name"], [req.body.name], function(reslt) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  let condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devourerd: req.body.devourerd
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
 module.exports = router;
